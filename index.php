db243988e88fd8fb6368b06f0f409d5ea704bd2a
ChristopherForsman <ch719forsman@gmail.com>
2018-01-16 12:30:46 +0200

    Laga en

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
 	include "includes/head.php"
 ?>
 
+<?php if ($_SESSION['username']) : ?>
 	<h1>Welcome, <?php echo $_SESSION['username']; ?></h1>
+	<a href="logout.php">Logout <?php echo $_SESSION['username']; ?></a>
+	
+<?php else : ?>
+	<h1>ACCESS DENIED</H1>
+<?php endif; ?>
 	
 </body>
 </html>
